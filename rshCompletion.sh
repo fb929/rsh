21d87c197cdaae442c3e2abf3b7207d5ca82851e
Grigory Efimov <grigory.efimov@gmail.com>
2025-04-26 03:58:46 +0000

    fixed rshCompletion

--- rshCompletion.sh
+++ rshCompletion.sh
@@ -1,4 +1,9 @@
-_rsh_completion(){
-    COMPREPLY=( $(compgen -W "$(rshCompgen.py)" -- "$2") )
+_rsh_completion_hosts(){
+    COMPREPLY=( $(compgen -W "$(rshCompgen.py hosts)" -- "$2") )
 }
-complete -F _rsh_completion sr
+_rsh_completion_groups(){
+    COMPREPLY=( $(compgen -W "$(rshCompgen.py groups)" -- "$2") )
+}
+complete -F _rsh_completion_hosts sr
+complete -F _rsh_completion_groups sExec
+complete -F _rsh_completion_groups pExec
